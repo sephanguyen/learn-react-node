0fc86bf946ab691532561e682ba49ab6696f9a02
Pha Nguyen <sephanguyen@gmail.com>
2018-05-17 10:43:25 +0700

    add components

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class AboutPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>React Redux and React Router</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class HomePage extends React.Component {
+
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight Administrator</h1>
+        <p>React Redux and React Router</p>
+        <Link to="about" className="btn btn-primary btn-lg"> Learn More </Link>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
